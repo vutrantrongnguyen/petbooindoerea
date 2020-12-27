77d794e238b752e19e73f0a14d0c19b4bb74e381
vutrantrongnguyen <quenmatkhaupart3@gmail.com>
2020-12-27 21:34:30 +0700

    add

--- src/pages/editProfile.js
+++ src/pages/editProfile.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { Table, Form, Button, Col } from "react-bootstrap"
+import React, { useEffect, useState } from "react"
+import { Table, Form, Button, Col, Modal } from "react-bootstrap"
 import { toast } from "react-toastify"
 
 const initialState = {
+  // "show": false,
   "div1": "",
   "div2": "none",
   "name": "Nguyen",
@@ -24,182 +25,270 @@ const initialState = {
   }],
 }
 
-class Profile extends React.Component {
-  // constructor() {
-  //   super()
-  //
-  // }
+const Profile = () => {
+  const [state, setState] = useState(initialState)
 
-  componentDidMount() {
-    if (typeof window !== "undefined") {
-      const storageState = window.localStorage.getItem("PROFILE")
-      if (!storageState) {
-        window.localStorage.setItem("PROFILE", JSON.stringify(initialState))
+  useEffect(() => {
+      if (typeof window !== "undefined") {
+        const storageState = window.localStorage.getItem("PROFILE")
+        if (!storageState) {
+          window.localStorage.setItem("PROFILE", JSON.stringify(initialState))
+        }
+        // this.state = JSON.parse(storageState)
+
       }
-      // this.state = JSON.parse(storageState)
+    }, [],
+  )
 
-    }
-  }
-
-  onChange = (e) => {
+  const onChange = (e) => {
     console.log({ [e.target.name]: e.target.value })
-    this.state[e.target.name] = e.target.value;
+    state[e.target.name] = e.target.value
     // this.setState({ [e.target.name]: e.target.value })
-    console.log(this.state)
   }
+  const [show, setShow] = useState(false)
 
-  saveProfile = (e) => {
-    window.localStorage.setItem("PROFILE", JSON.stringify(this.state))
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+
+  const saveProfile = (e) => {
+    window.localStorage.setItem("PROFILE", JSON.stringify(state))
     toast("Successfully update profile!", {
       position: toast.POSITION.TOP_LEFT,
     })
     this.forceUpdate()
   }
 
-  Edit = (e) => {
+  const Edit = (e) => {
     this.setState({ div1: "none", div2: "" })
   }
-  Edit2 = (e) => {
+  const Edit2 = (e) => {
     // this.setState({ div1: "", div2: "none" })
-    window.localStorage.setItem("PROFILE", JSON.stringify(this.state))
+    window.localStorage.setItem("PROFILE", JSON.stringify(state))
     toast("Successfully update profile!", {
       position: toast.POSITION.TOP_LEFT,
     })
     this.forceUpdate()
-    window.location.href="/profile"
+    window.location.href = "/profile"
   }
 
-  render() {
-    this.state = initialState
-    if (typeof window !== "undefined") {
-      const storageState = window.localStorage.getItem("PROFILE")
-      if (storageState) {
-        this.state = JSON.parse(storageState)
-      }
-    }
-    return (
-      <div>
-        <div className="flex flex-1 justify-center">
-          <div className="w-full max-w-144">
-            <strong>Profile</strong>
+  const handleDelete = (e) => {
+    this.setState({
+      ...state,
+      pet: state.pet.splice(e, 1),
+    })
+    console.log(e)
+    window.localStorage.setItem("PROFILE", JSON.stringify(state))
+    toast("Successfully update profile!", {
+      position: toast.POSITION.TOP_LEFT,
+    })
+    this.forceUpdate()
+
+  }
+
+
+  // this.state = initialState
+  // let check = this.state.show
+
+  // if (typeof window !== "undefined") {
+  //   const storageState = window.localStorage.getItem("PROFILE")
+  //   if (storageState) {
+  //     this.state = JSON.parse(storageState)
+  //   }
+  // }
+  return (
+    <div>
+      <div className="flex flex-1 justify-center">
+        <div className="w-full max-w-144">
+          <strong>Profile</strong>
+          <hr />
+          <div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                Username
+              </label>
+              <input
+                onChange={onChange} name="name"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username" type="text" placeholder={state.name} />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                onChange={onChange} name="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="email" placeholder={state.email} />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+                Phone Number
+              </label>
+              <input
+                onChange={onChange} name="phone"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone" type="text" placeholder={state.phone} />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                onChange={onChange} name="password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password" type="password" placeholder="******************" />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Re-enter Password
+              </label>
+              <input
+                onChange={onChange} name="password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password" type="password" placeholder="******************" />
+            </div>
+            <strong>Your pets</strong>
             <hr />
-            <div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                  Username
-                </label>
-                <input
-                  onChange={this.onChange} name="name"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username" type="text" placeholder={this.state.name} />
+
+            <Form>
+              {state.pet.map((item, index) => {
+                  return (<>
+                      <Form.Row>
+                        <Form.Group as={Col}>
+                          <Form.Label>Name</Form.Label>
+                          <Form.Control placeholder={item.name} />
+                        </Form.Group>
+
+                        <Form.Group as={Col}>
+                          <Form.Label>Type</Form.Label>
+                          <Form.Control placeholder={item.type} />
+                        </Form.Group>
+
+                        <Form.Group as={Col}>
+                          <Form.Label>YoB</Form.Label>
+                          <Form.Control as="select" defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                          </Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                      <Form.Row>
+                        <Form.Group as={Col}>
+                          <Form.Label>Weight</Form.Label>
+                          <Form.Control type="email" placeholder={item.weight} />
+                        </Form.Group>
+
+                        <Form.Group as={Col}>
+                          <Form.Label>Sex</Form.Label>
+                          <Form.Control as="select" defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option>male</option>
+                            <option>female</option>
+                          </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col}>
+                          <Form.Label></Form.Label>
+                          <Button onClick={() => handleDelete(index)}>Delete</Button>
+                        </Form.Group>
+                      </Form.Row>
+
+                    </>
+                  )
+                },
+              )}
+
+              <div style={{ justifyContent: "center", display: "flex" }}>
+
+                <Button onClick={handleShow}
+                        variant="light">
+                  <img src="/images/products/add.png" width="50px" alt="" />
+                </Button>
               </div>
-              <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                  Email
-                </label>
-                <input
-                  onChange={this.onChange} name="email"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="email" placeholder={this.state.email} />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
-                  Phone Number
-                </label>
-                <input
-                  onChange={this.onChange} name="phone"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="phone" type="text" placeholder={this.state.phone} />
-              </div>
-              <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  onChange={this.onChange} name="password"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password" type="password" placeholder="******************" />
-              </div>
-              <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                  Re-enter Password
-                </label>
-                <input
-                  onChange={this.onChange} name="password"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password" type="password" placeholder="******************" />
-              </div>
-              <strong>Your pets</strong>
-              <hr />
-
-              <Form>
-                {this.state.pet.map((item) => {
-                    return (<>
-                        <Form.Row>
-                          <Form.Group as={Col} controlId="formGridCity">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control placeholder={item.name}/>
-                          </Form.Group>
-
-                          <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>Type</Form.Label>
-                            <Form.Control placeholder={item.type}/>
-                          </Form.Group>
-
-                          <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>YoB</Form.Label>
-                            <Form.Control as="select" defaultValue="Choose...">
-                              <option>Choose...</option>
-                              <option>1</option>
-                              <option>2</option>
-                              <option>3</option>
-                              <option>4</option>
-                              <option>5</option>
-                              <option>6</option>
-                              <option>7</option>
-                              <option>8</option>
-                            </Form.Control>
-                          </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                          <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Label>Weight</Form.Label>
-                            <Form.Control type="email" placeholder={item.weight}/>
-                          </Form.Group>
-
-                          <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label>Sex</Form.Label>
-                            <Form.Control as="select" defaultValue="Choose...">
-                              <option>Choose...</option>
-                              <option>male</option>
-                              <option>female</option>
-                            </Form.Control>
-                          </Form.Group>
-
-                          <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label></Form.Label>
-                            <Button>Delete</Button>
-                          </Form.Group>
-                        </Form.Row>
-
-                      </>
-                    )
-                  },
-                )}
-                <Button variant="primary" onClick={this.Edit2}>
+              <div style={{ justifyContent: "center", display: "flex" }}>
+                <Button variant="primary" onClick={Edit2}>
                   Save
                 </Button>
-              </Form>
-            </div>
-          </div>
+              </div>
+            </Form>
+            <Modal show={show} onHide={handleClose} animation={false}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add Pet</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <>
+                    <Form.Row>
+                      <Form.Group as={Col}>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control />
+                      </Form.Group>
 
+                      <Form.Group as={Col}>
+                        <Form.Label>Type</Form.Label>
+                        <Form.Control />
+                      </Form.Group>
+
+                      <Form.Group as={Col}>
+                        <Form.Label>YoB</Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                          <option>Choose...</option>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                        </Form.Control>
+                      </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                      <Form.Group as={Col}>
+                        <Form.Label>Weight</Form.Label>
+                        <Form.Control type="email" />
+                      </Form.Group>
+
+                      <Form.Group as={Col}>
+                        <Form.Label>Sex</Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                          <option>Choose...</option>
+                          <option>male</option>
+                          <option>female</option>
+                        </Form.Control>
+                      </Form.Group>
+                    </Form.Row>
+                  </>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2020 Preslyyyyy. All rights reserved.
-        </p>
+
       </div>
-    )
-  }
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2020 Preslyyyyy. All rights reserved.
+      </p>
+    </div>
+  )
+
 }
 
 export default Profile
